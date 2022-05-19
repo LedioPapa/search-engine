82c71eeaad05d3e4ac4ace0373db20262599ccbe
papa <ledio.papa@sisal.al>
2022-05-19 11:51:20 +0200

    codacy styling

--- src/main/java/com/se/searchengine/repository/DocumentRepository.java
+++ src/main/java/com/se/searchengine/repository/DocumentRepository.java
@@ -4,12 +4,6 @@ import com.se.searchengine.model.Document;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface DocumentRepository extends ElasticsearchRepository<Document, Long> {
-
-    default List<Document> getDocumentsByTokensContaining(String expression){
-        return null;
-    }
 }
